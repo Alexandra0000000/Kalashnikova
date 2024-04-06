d665d87af15bfd6e59f3da6ced158363a500da29
Alexandra0000000 <skalasnikova103@gmail.com>
2024-04-06 11:49:11 +0300

    rr

---
+++ PZ15/data_tovar.py
@@ -0,0 +1,12 @@
+info_tovar = [
+    (1, 425, 'Puma', 'shoes', 7000, 12, 2),
+    (2, 647, 'Adidas', 'shoes', 6500, 31, 5),
+    (3, 837, 'Nike', 'shoes', 6800, 11, 6),
+    (4, 590, 'Asics', 'shoes', 4000, 6, 3),
+    (5, 435, 'Cappa', 't-shirts', 2500, 7, 4),
+    (6, 735, 'Manto', 't-shirts', 2700, 23, 15),
+    (7, 677, 'Rebook', 't-shirts', 1800, 15, 7),
+    (8, 315, 'Diesel', 'handbags', 17000, 3, 1),
+    (9, 594, 'Lacoste', 'perfume', 8000, 8, 3),
+    (10, 631, 'Versace', 'perfume', 9000, 8, 4),
+]
---
+++ PZ15/pz1.py
@@ -0,0 +1,68 @@
+# Приложение ТОВАРНЫЙ ЗАПАС для автоматизированного учета товарных запасов на складе.
+# БД должна содержать таблицу Товары со следующей структурой записи:
+# Код товара, Торговая марка, Тип, Цена, Количество на складе, Минимальный запас.
+
+import sqlite3 as sq
+from data_tovar import info_tovar
+
+with sq.connect('tovarny_zapas.db') as con:
+    cur = con.cursor()
+
+    cur.execute("DROP TABLE IF EXISTS tovary")
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS tovary(
+    tovary_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    kod_tovara INTEGER,
+    marka TEXT NOT NULL,
+    type TEXT NOT NULL,
+    price INTEGER,
+    kolvo INTEGER,
+    min_zapas INTEGER 
+    )""")
+
+    with sq.connect('tovarny_zapas.db') as con:
+        cur = con.cursor()
+        cur.executemany("INSERT INTO tovary VALUES (?, ?, ?, ?, ?, ?, ?)", info_tovar)
+
+
+with sq.connect('tovarny_zapas.db') as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM tovary WHERE kolvo > 20 AND min_zapas < 7")
+    result = cur.fetchall()
+    print(result)
+
+with sq.connect('tovarny_zapas.db') as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM tovary WHERE price > 8000")
+    result = cur.fetchall()
+    print(result)
+
+with sq.connect('tovarny_zapas.db') as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM tovary WHERE min_zapas < 4")
+    result = cur.fetchall()
+    print(result)
+
+with sq.connect('tovarny_zapas.db') as con:
+    cur = con.cursor()
+    cur.execute("UPDATE tovary SET price = price+300 WHERE marka LIKE 'A%'")
+    result = cur.fetchall()
+    print(result)
+
+with sq.connect('tovarny_zapas.db') as con:
+    cur = con.cursor()
+    cur.execute("UPDATE tovary SET kolvo = kolvo+3 WHERE type LIKE 'shoes'")
+    result = cur.fetchall()
+    print(result)
+
+with sq.connect('tovarny_zapas.db') as con:
+    cur = con.cursor()
+    cur.execute("UPDATE tovary SET min_zapas = min_zapas+1 WHERE type LIKE 't-shirts'")
+    result = cur.fetchall()
+    print(result)
+
+with sq.connect('tovarny_zapas.db') as con:
+    cur = con.cursor()
+    cur.execute("DELETE FROM tovary WHERE tovary_id = 9")
+    result = cur.fetchall()
+    print(result)
